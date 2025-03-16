42a56db3bf9832e16f87d8f2c196fe8fed386049
Finn Milner <finn.milner@outlook.com>
2025-03-16 14:42:07 +0000

    fix: ensure directory exists before saving to file

--- src/save.ts
+++ src/save.ts
@@ -3,6 +3,7 @@
 import env from "../env"
 import { fileURLToPath, URL } from "node:url"
 import * as fs from "node:fs"
+import * as path from "node:path"
 import type { SpecialOffer } from "../.d.ts"
 
 // #endregion Imports
@@ -15,6 +16,13 @@ const fileURL = new URL(`../data/${env.MODE}.json`, import.meta.url)
 
 // Write data to local file
 export const save = (offers: SpecialOffer[]) => {
+	// Get directory path
+	const dir = path.dirname(fileURLToPath(fileURL))
+
+	// Ensure directory exists
+	if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true })
+
+	// Write data to file
 	fs.writeFile(fileURL, JSON.stringify(offers, null, 4), (err) => {
 		if (err) throw err
 	})
