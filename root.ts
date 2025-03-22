f3a212e8276091c767f9cc672af145145bb1846a
Finn Milner <finn.milner@outlook.com>
2025-03-22 11:10:29 +0000

    fix: use platform compatible method of converting path to URL

--- root.ts
+++ root.ts
@@ -1,23 +1,13 @@
 // #region Imports
 
-import { URL } from "node:url"
+import { URL, pathToFileURL } from "node:url"
 
 // #endregion Imports
 
 
 
-// ISSUE: May only work on windows
-const execPathToUrl = (path: string) => {
-	const formatted = "file:///" + path
-		.replaceAll("\\", "/")
-
-	return new URL(formatted)
-}
-
-
-
-const runningAsExe = Deno.execPath().endsWith("scraper.exe")
-const denoExecUrl = execPathToUrl(Deno.execPath())
+const runningAsExe = !(Deno.execPath().endsWith("deno.EXE"))
+const denoExecUrl = pathToFileURL(Deno.execPath())
 
 export const rootUrl = runningAsExe
 	? new URL("./", denoExecUrl)
